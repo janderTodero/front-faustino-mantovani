efd7ab5d3b360b05c4186ab33e97decea31a3ace
janderTodero <jandertodero@gmail.com>
2025-09-09 16:57:25 -0300

    favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
   return <Component {...pageProps} />
 }
 
